2c70cd1689194b0ad454b8d27b7c787225360876
Josh Cheek <josh.cheek@gmail.com>
2017-01-13 14:46:48 -0600

    Got the websocket example working

--- examples/Websocket.elm
+++ examples/Websocket.elm
@@ -1,39 +1,64 @@
-import Html exposing (ul, li, input, button, text, div)
+module Main exposing (..)
+
+import Html exposing (Html, ul, li, input, button, text, div)
 import Html.Attributes exposing (value, placeholder)
+import Html.Events exposing (onClick, onInput)
+import WebSocket
+
 
 main =
-  Html.program
-  { init = init
-  , update = update
-  , view = view
-  , subscriptions = subscriptions
-  }
+    Html.program
+        { init = init
+        , update = update
+        , view = view
+        , subscriptions = subscriptions
+        }
+
 
 type alias Model =
-  { message : String
-  , messages : List String
-  }
+    { message : String
+    , messages : List String
+    }
+
 
 type Msg
-  = NoMsg
+    = SendWsMessage String
+    | ReceiveWsMessage String
+    | UpdateMessage String
+
+
+websocketUrl =
+    "ws://echo.websocket.org"
+
 
 init =
-  (Model "" [], Cmd.none)
+    ( Model "" [], Cmd.none )
+
 
 update msg model =
-  case msg of
-    NoMsg -> (model, Cmd.none)
+    case msg of
+        UpdateMessage toUpdate ->
+            ( { model | message = toUpdate }, Cmd.none )
+
+        SendWsMessage toSend ->
+            ( { model | message = "" }, WebSocket.send websocketUrl toSend )
+
+        ReceiveWsMessage received ->
+            ( { model | messages = (received :: model.messages) }, Cmd.none )
+
 
 subscriptions model =
-  Sub.none
+    WebSocket.listen websocketUrl ReceiveWsMessage
 
+
+view : Model -> Html Msg
 view model =
-  let
-      viewMessage message =
-        li [] [text message]
-  in
-    div []
-      [ input [placeholder "Message", value model.message] []
-      , button [] [text "send"]
-      , ul [] (List.map viewMessage model.messages)
-      ]
+    let
+        viewMessage message =
+            li [] [ text message ]
+    in
+        div []
+            [ input [ placeholder "Message", value model.message, onInput UpdateMessage ] []
+            , button [ onClick (SendWsMessage model.message) ] [ text "send" ]
+            , ul [] (List.map viewMessage model.messages)
+            ]
