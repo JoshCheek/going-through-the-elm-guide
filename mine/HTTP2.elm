666a1d64fd5bf758dbad64e3ef607f130e6f4afd
Josh Cheek <josh.cheek@gmail.com>
2017-01-13 14:08:46 -0600

    Basic functionality in place for v2 of http

--- mine/HTTP2.elm
+++ mine/HTTP2.elm
@@ -1,19 +1,24 @@
 module Main exposing (..)
 
-import Html exposing (Html, div, h2, button, img, text)
+import Html exposing (Html, div, h2, button, img, text, button)
 import Html.Attributes exposing (src)
+import Html.Events exposing (onClick)
+import Http
+import Json.Decode
 
 type alias ImageGenerator =
   { topic : String
   , imageUrl : String
   }
 
-type Msg =
-  NoMsg
+type Msg
+  = NoMsg
+  | GetRandomImage
+  | ReplaceGif String
 
 main =
   Html.program
-  { init = init "cats"
+  { init = init "dogs"
   , view = view
   , update = update
   , subscriptions = subscriptions
@@ -22,21 +27,39 @@ main =
 defaultImageUrl =
   "https://upload.wikimedia.org/wikipedia/commons/3/3f/No-red.svg"
 
+init : String -> (ImageGenerator, Cmd Msg)
 init topic =
-  ( ImageGenerator topic defaultImageUrl, Cmd.none )
+  ( ImageGenerator topic defaultImageUrl, getRandomImage topic )
 
-update msg model =
-  (model, Cmd.none)
+update : Msg -> ImageGenerator -> (ImageGenerator, Cmd Msg)
+update msg imgGen =
+  case msg of
+    NoMsg -> (imgGen, Cmd.none)
+    GetRandomImage -> (imgGen, getRandomImage imgGen.topic)
+    ReplaceGif newUrl -> ({ imgGen | imageUrl = newUrl }, Cmd.none)
 
+getRandomImage : String -> Cmd Msg
+getRandomImage topic =
+  let
+    url = "https://api.giphy.com/v1/gifs/random?api_key=dc6zaTOxFJmzC&tag=" ++ topic
+    getGif = Http.get url decodeGifUrl
+    decodeGifUrl = Json.Decode.at ["data", "image_url"] Json.Decode.string
+    replaceGif result =
+      case result of
+        Err _ -> NoMsg
+        Ok url -> ReplaceGif url
+  in
+    Http.send replaceGif getGif
 
-subscriptions model =
+subscriptions : ImageGenerator -> Sub Msg
+subscriptions imgGen =
   Sub.none
 
 
 view : ImageGenerator -> Html Msg
-view model =
+view imgGen =
   div []
-    [ h2 [] [text model.topic]
-    , img [src model.imageUrl] []
+    [ h2 [] [text imgGen.topic]
+    , button [ onClick GetRandomImage ] [ text "Get Random Image" ]
+    , img [src imgGen.imageUrl] []
     ]
-
